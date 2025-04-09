65315488433ffed90155b9d6c9a8ad957dcc74c6
Thiago Guimaraes <thiagotguimaraes@gmail.com>
2025-04-09 13:51:58 +0400

    RAG pipeline working

---
+++ openai_client.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+# Load environment variables. Assumes that project contains .env file with API keys
+load_dotenv()
+
+# To authenticate with the model you will need to generate a personal access token (PAT) in your GitHub settings. 
+# Create your PAT token by following instructions here: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
+client = OpenAI(
+    base_url="https://models.inference.ai.azure.com",
+    api_key=os.environ["GITHUB_TOKEN"],
+)
